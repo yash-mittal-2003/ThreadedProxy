8b01467859847b244b51c9b9cbc3c2e3831bae65
Yash Mittal <110320507+yash-mittal-2003@users.noreply.github.com>
2024-10-10 21:16:02 +0530

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,6 +1,6 @@
 """
 ******************************************************************************
-Author      = Yash Mittal (112101054), Rachit Jain (112101019),
+Author      = Yash Mittal (112101054), Rachit Jain (112101019)
 
 Project     = HTTP Proxy Server
 
